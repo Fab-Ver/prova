c1d398063624922e8c237e8350d7784869364546
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-10 10:35:39 +0100

    a caso

---
+++ src/prova/GameView.java
@@ -0,0 +1,15 @@
+package prova;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameView extends JFrame {
+	
+	
+	public GameView() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(1920,1080);
+		this.setVisible(true);
+	}
+	
+}
